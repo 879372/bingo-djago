14188dc3c22b2cb0ca179e9099c3f414fe6ffa77
José Kaio <126286274+879372@users.noreply.github.com>
2024-05-13 17:43:06 -0300

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -35,16 +35,18 @@ def bingo_cards(request):
         numeros_sorteados, _ = sortear_numeros()  # Não precisamos dos números disponíveis aqui
         
         sorteio = NumeroSorteado.objects.create(numeros=numeros_sorteados)
-
         cartelas = []
-        cartela_completa = None
+
+        # Variáveis para armazenar informações sobre as cartelas vencedoras
+        primeira_cartela_linhas_completas = None
+        primeira_cartela_linhas_min_numeros = None
+        primeira_cartela_cartela_completa = None
 
         for _ in range(quantidade_cartelas):
             numeros_cartela = gerar_cartela()
             numeros_gerados_str = json.dumps(numeros_cartela)
             nova_cartela = Cartela.objects.create(jogador=jogador, numeros_gerados=numeros_gerados_str, id_sorteio=sorteio)
             cartelas.append(nova_cartela)
-
 
         for cartela in cartelas:
             cartela_json = cartela.numeros_gerados
@@ -54,37 +56,58 @@ def bingo_cards(request):
             lista_col3 = cartela_dict['lin3']
             todas_listas = [lista_col1, lista_col2, lista_col3]
             numeros_sem_travessoes = []
+
+            linhas_completas = any(all(numero in numeros_sorteados for numero in linha) for linha in todas_listas)
+            linhas_min_numeros = any(sum(numero in numeros_sorteados for numero in linha) >= 4 for linha in todas_listas)
+            cartela_completa = all(numero in numeros_sorteados for linha in todas_listas for numero in linha)
+
             for lista in todas_listas:
                 numeros_sem_travessoes.extend([numero for numero in lista if isinstance(numero, int)])
             cartela.numeros_gerados = numeros_sem_travessoes
             cartela.numeros_gerados = str(cartela.numeros_gerados)[1:-1]
             cartela.numeros_gerados = cartela.numeros_gerados.split(',')
-            #print(cartela.numeros_gerados)
 
-        return render(request, 'bingo/bingo_cards.html', {'cartelas': cartelas, 'cartela_completa': cartela_completa, 'numeros_sorteados': numeros_sorteados})
+            # Lógica para verificar a primeira cartela a alcançar cada estado vencedor
+            if linhas_completas and primeira_cartela_linhas_completas is None:
+                primeira_cartela_linhas_completas = cartela
+            if linhas_min_numeros and primeira_cartela_linhas_min_numeros is None:
+                primeira_cartela_linhas_min_numeros = cartela
+            if cartela_completa and primeira_cartela_cartela_completa is None:
+                primeira_cartela_cartela_completa = cartela
+            
+            print(todas_listas)
+            print(primeira_cartela_linhas_completas, '1')
+            print(primeira_cartela_linhas_min_numeros, '2')
+            print(primeira_cartela_cartela_completa, '3')
+        return render(request, 'bingo/bingo_cards.html', {'cartelas': cartelas, 'primeira_cartela_linhas_completas': primeira_cartela_linhas_completas, 'primeira_cartela_linhas_min_numeros': primeira_cartela_linhas_min_numeros, 'primeira_cartela_cartela_completa': primeira_cartela_cartela_completa, 'numeros_sorteados': numeros_sorteados})
     else:
         return render(request, 'bingo/home.html')
 
 
 
+def verificar_estado_cartela(cartela, numeros_sorteados, min_numeros):
+        linhas_completas = any(all(numero in numeros_sorteados for numero in linha) for linha in cartela.numeros_gerados.values())
+        linhas_min_numeros = any(sum(numero in numeros_sorteados for numero in linha) >= 4 for linha in cartela.numeros_gerados.values())
+        cartela_completa = all(numero in numeros_sorteados for linha in cartela.numeros_gerados.values() for numero in linha)
+        print(linhas_completas)
+        print(linhas_min_numeros)
+        print(cartela_completa)
+        return linhas_completas, linhas_min_numeros, cartela_completa
+
+
+
 def salvar_numeros_sorteados(request):
-    if request.method == 'POST':
-        # Obtenha os números sorteados do corpo da solicitação POST
-        numeros_sorteados = request.POST.getlist('numeros_sorteados[]')
-        print(numeros_sorteados)
-        # Obter o último sorteio
-        sorteio = NumeroSorteado.objects.latest('id')
-        print(sorteio)
-        # Use o ID do último sorteio para filtrar as cartelas associadas a esse sorteio
-        cartelas_do_sorteio = Cartela.objects.filter(id_sorteio_id=sorteio.id)
-        print(cartelas_do_sorteio)
+    ultimo_sorteio = NumeroSorteado.objects.latest('id')
+    cartelas_do_sorteio = Cartela.objects.filter(id_sorteio_id=ultimo_sorteio.id)
         # Iterar sobre as cartelas encontradas
-        for cartela in cartelas_do_sorteio:
-            numeros_gerados = cartela.numeros_gerados
-            print(numeros_gerados)   
-        return JsonResponse({'message': 'Nenhum jogador teve todos os números da cartela sorteados.'})
-    else:
-        return HttpResponse('Método não permitido', status=405)
+    for cartela in cartelas_do_sorteio:
+        numeros_gerados = cartela.numeros_gerados
+        print(numeros_gerados)  
+
+    sorteio = NumeroSorteado.objects.get(id=ultimo_sorteio.id)
+    print(sorteio.numeros)
+
+    return render(request, 'bingo/bingo_cards.html', {'message': 'Nenhum jogador teve todos os números da cartela sorteados.'})
 
 
 
